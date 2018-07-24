054a15ebee51d55dce6648e4a48ba3be2a4e6c6a
flippingflippers <citrussunshine2004@gmail.com>
2018-07-24 14:57:27 +0000

    Done.

---
+++ new_meal_choice.rb
@@ -0,0 +1,20 @@
+def breakfast (default_food= "frosted flakes")
+  "Morning is the most spectacular time of the day for nice healthy #{default_food}"
+end
+
+def lunch (default_food= "grilled cheese")
+  "Around 12'o clock thats the time when you need to re energize and fill up on some #{default_food}"
+end
+
+def snack (default_food= "trail mix")
+  "Hungry before the final course? Thats ok, snack on some #{default_food} to keep you up and at'em for the rest of your day!"
+end
+
+def dinner (default_food= "salmon")
+  "The last meal of the day should be your best, so some nice homecooked #{default_food} should be just the right thing to finish up your night."
+end
+
+breakfast("pancakes")
+lunch("macaroni")
+snack("fruit snacks")
+dinner("roast beef stew")
